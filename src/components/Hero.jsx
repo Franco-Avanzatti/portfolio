69ac2990620b6386f40437232dd08d40ab6e2031
Franco Avanzatti <avanzatti5@gmail.com>
2025-06-17 18:38:51 -0300

    foto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import image from "/image.jpg"
+import image from "/perfil.jpeg"
 import { motion } from "framer-motion" 
 
 const Hero = () => {
@@ -13,7 +13,7 @@ const Hero = () => {
         transition={{duration: 0.8,delay:0.2}}
 
         >
-          <img src={image} alt="" className="w-[300px] cursor-pointer rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2x1 hover:shadow-indigo-600 md:w-[350px]"/>
+          <img src={image} alt="" className="w-[200px] h-[400px] cursor-pointer rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2x1 hover:shadow-indigo-600 md:w-[350px]"/>
         </motion.div>
         
 
